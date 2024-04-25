85774893fe8a03a13df14d697f9a684a6616123b
tarie100 <tarie.school@gmail.com>
2024-04-25 05:31:47 -0700

    0. Minimum Operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+How many operations?
+"""
+
+
+def minOperations(n):
+    """
+    a method that calculates the fewest number of operations
+    needed to result in exactly n H characters in the file.
+    """
+
+    if n == 1:
+        return 0
+    dp = [float('inf')] * (n + 1)
+    dp[1] = 0
+
+    for i in range(2, n + 1):
+        for j in range(1, i):
+            if i % j == 0:
+                dp[i] = min(dp[i], dp[j] + i // j)
+    return dp[n] if dp[n] != float('inf') else 0
