65d7dcd84accf8f3be8da49937f599ac2c3dd68f
tarie100 <tarie.school@gmail.com>
2024-05-15 04:06:55 -0700

    0. N queens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -27,17 +27,12 @@ def isSafe(board, row, col):
      for i in range(col):
         if board[row][i] == 1:
             return False
-    
-    # Check upper diagonal on left side
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-    
-    # Check lower diagonal on left side
-    for i, j in zip(range(row, N, 1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-    
+        for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+            if board[i][j] == 1:
+                return False
+        for i, j in zip(range(row, N, 1), range(col, -1, -1)):
+            if board[i][j] == 1:
+                return False
     return True
 
 def solveNQUtil(board, col):
