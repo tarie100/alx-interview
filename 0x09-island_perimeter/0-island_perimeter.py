af56111ed7c68734d78c9de95b006f31afac189f
tarie100 <tarie.school@gmail.com>
2024-06-12 04:08:18 -0700

    Island Perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -9,27 +9,17 @@ def island_perimeter(grid):
     returns the perimeter of 
     the island described in grid
     """
-    perimeter = 0
-
-    # Get the dimensions of the grid
-    rows, cols = len(grid), len(grid[0])
-
-    # Iterate through each cell in the grid
-    for i in range(rows):
-        for j in range(cols):
-            if grid[i][j] == 1:
-                # Count the number of adjacent water cells
-                adjacent_water = 0
-                if i > 0 and grid[i - 1][j] == 0:
-                    adjacent_water += 1
-                if i < rows - 1 and grid[i + 1][j] == 0:
-                    adjacent_water += 1
-                if j > 0 and grid[i][j - 1] == 0:
-                    adjacent_water += 1
-                if j < cols - 1 and grid[i][j + 1] == 0:
-                    adjacent_water += 1
-
-                # Add the perimeter contribution for this cell
-                perimeter += adjacent_water
-
-    return perimeter
+    
+     p = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if (grid[i][j] == 1):
+                if (i <= 0 or grid[i - 1][j] == 0):
+                    p += 1
+                if (i >= len(grid) - 1 or grid[i + 1][j] == 0):
+                    p += 1
+                if (j <= 0 or grid[i][j - 1] == 0):
+                    p += 1
+                if (j >= len(grid[i]) - 1 or grid[i][j + 1] == 0):
+                    p += 1
+    return p
